d6e4f50ec2052465b7e768306373bcc6d94f669b
prathmesh <prathmeshpol61@gmail.com>
2024-01-24 14:44:58 +0530

    add  - login route added

--- backend/src/controllers/auth-controller.ts
+++ backend/src/controllers/auth-controller.ts
@@ -7,6 +7,10 @@ const userSchema = z.object({
   email: z.string().email(),
   password: z.string().min(5, "password is required"),
 });
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(5, "password is required"),
+});
 export const registerUser = async (req: Request, res: Response) => {
   try {
     const body = req.body;
@@ -16,13 +20,13 @@ export const registerUser = async (req: Request, res: Response) => {
       return res.json(validation.error.format());
     }
     // check user already register
-    const userExcist = await prisma.user.findFirst({
+    const userExist = await prisma.user.findFirst({
       where: {
         email: validation.data.email,
       },
     });
-    if (userExcist) {
-      return res.status(400).json("User already excist login");
+    if (userExist) {
+      return res.status(400).json("User already Exist login");
     }
 
     // register user
@@ -42,5 +46,46 @@ export const registerUser = async (req: Request, res: Response) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json("somthing went wrong in register.");
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const validation = loginSchema.safeParse(body);
+    if (!validation.success) {
+      return res.json(validation.error.format());
+    }
+    // check user already register or not registered
+    const userExist = await prisma.user.findFirst({
+      where: {
+        email: validation.data.email,
+      },
+    });
+    if (!userExist) {
+      return res.status(400).json("User not register");
+    }
+
+    // check password match
+    const password = validation.data.password === userExist.password;
+    if (!password) {
+      return res.status(400).json("User unauthorized check login credentials");
+    }
+    // token created
+    const token = jwt.sign(
+      { userID: userExist.email },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: "1hr",
+      }
+    );
+
+    return res
+      .cookie("access_token", token)
+      .status(201)
+      .json("User login successful");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("somthing went wrong in login.");
   }
 };
--- backend/src/router/auth-route.ts
+++ backend/src/router/auth-route.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
-import { registerUser } from "../controllers/auth-controller";
+import { loginUser, registerUser } from "../controllers/auth-controller";
 
 const router = express.Router();
 
 // register route - /api/register
 router.post("/register", registerUser);
+// login route - /api/login
+router.post("/login", loginUser);
 
 export default router;
